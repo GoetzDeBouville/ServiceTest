d644c03ae8e3537e2514381cd122b6ea5226a577
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-22 23:37:45 +0300

    выносит работу MusicService в фоновый поток

--- app/src/main/java/com/hellcorp/servicetest/MusicService.kt
+++ app/src/main/java/com/hellcorp/servicetest/MusicService.kt
@@ -4,12 +4,19 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import androidx.annotation.MainThread
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MusicService : Service() {
     private var player: ExoPlayer? = null
+    private var serviceJob : Job? = null
 
     override fun onBind(intent: Intent?): IBinder? = null
 
@@ -23,6 +30,7 @@ class MusicService : Service() {
 
     override fun onDestroy() {
         player?.release()
+        serviceJob?.cancel()
         stopSelf()
     }
 
@@ -32,7 +40,9 @@ class MusicService : Service() {
         val songUrl = intent?.getStringExtra(MainActivity.SONG_URL_KEY)
         if (songUrl != null) {
             Log.d(LOG_TAG, "onStartCommand -> song url exists")
-            prepareAndStartPlayer(songUrl)
+            serviceJob = CoroutineScope(Dispatchers.Default).launch {
+                prepareAndStartPlayer(songUrl)
+            }
         }
         Log.i(LOG_TAG, "Process = ${android.os.Process.myPid()}")
 
@@ -40,11 +50,13 @@ class MusicService : Service() {
         return Service.START_REDELIVER_INTENT
     }
 
-    private fun prepareAndStartPlayer(songUrl: String) {
-        val mediaItem = MediaItem.fromUri(songUrl)
-        player?.addMediaItem(mediaItem)
-        player?.prepare()
-        player?.play()
+    private suspend fun prepareAndStartPlayer(songUrl: String) {
+        withContext(Dispatchers.Main) {
+            val mediaItem = MediaItem.fromUri(songUrl)
+            player?.addMediaItem(mediaItem)
+            player?.prepare()
+            player?.play()
+        }
     }
 
     private val playerListener = object : Player.Listener {
